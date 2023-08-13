670be326a2cf9abe644a9c813c6015061413f08e
teresacheca <teresa.checamarabotto@gmail.com>
2023-08-13 13:39:56 +0200

    Comentarios admi-general

--- frontend/src/app/admi-general/admi-general.component.ts
+++ frontend/src/app/admi-general/admi-general.component.ts
@@ -12,6 +12,7 @@ export class AdmiGeneralComponent {
 
   constructor(private reservaServices: ReservasService, private router: Router, private activeRoute: ActivatedRoute ){}
 
+  //Objeto de tipo usuario donde guardaremos los datos del usuario con el que estamos operando
   admi_general: Usuario = {
     nombre_usuario: '',
     contrasena: '',
@@ -27,19 +28,24 @@ export class AdmiGeneralComponent {
   aux: any = []
 
   ngOnInit(){
+    //Cogemos los parámetros que se leen en la url
     const params = this.activeRoute.snapshot.params;
     this.nombre_admi = params["nombre_usuario"]
+
+    //Comprobamos que el usuario haya iniciado sesión
     this.reservaServices.getLogin(params["nombre_usuario"], params["contrasena"]).subscribe(
       res => {
-        //this.reserva = res; //no funciona -> tiene que aparecer la información antigua para editar sobre ella
+        //Guardamos los datos que nos interesan del usuario
         this.admi_general.nombre_usuario = params["nombre_usuario"]
         this.admi_general.contrasena = params["contrasena"]
         this.admi_general.id = params["id"]
         this.admi_general.empresa = params["empresa"]
+        //Obtenemos todos los datos del usuario para poder operar con estos a partir de su nombre
         this.reservaServices.getUsuarioNombre(this.admi_general.nombre_usuario).subscribe(
           res => {
-           this.aux = res
-           this.admi_general.nombre_usuario = this.aux[0].nombre_usuario
+            //Copiamos todos los datos que nos devuelve la función en un objeto de tipo usuario que hemos llamado admi_general
+            this.aux = res
+            this.admi_general.nombre_usuario = this.aux[0].nombre_usuario
             this.admi_general.contrasena = this.aux[0].contrasena
             this.admi_general.tipo = this.aux[0].tipo
             this.admi_general.fecha_nacimiento = this.aux[0].fecha_nacimiento
@@ -56,11 +62,13 @@ export class AdmiGeneralComponent {
     
   }
 
+  //Función que nos lleva a la ruta que nos muestra la lista de las empresas dadas ya de alta en la aplicación
   mostrarEmpresas(){
     let ruta = '/reservas/' + this.nombre_admi + '/empresas'
     this.router.navigate([ruta]);
   }
 
+  //Función que nos lleva a la ruta que nos muestra la lista de todas las solicitudes
   mostrarSolicitudes(){
     let ruta = '/reservas/' + this.nombre_admi + '/lista_solicitudes'
     this.router.navigate([ruta]);

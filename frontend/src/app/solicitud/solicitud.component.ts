c2a4423f17b02fdf1a532ee5c177dd01287f3fee
teresacheca <teresa.checamarabotto@gmail.com>
2023-08-14 20:17:16 +0200

    Comentarios solicitud

--- frontend/src/app/solicitud/solicitud.component.ts
+++ frontend/src/app/solicitud/solicitud.component.ts
@@ -11,6 +11,7 @@ import {Empresa} from '../modelos/Empresas' ///importar la interfaz
 export class SolicitudComponent {
   constructor(private router: Router, private activeRoute: ActivatedRoute, private reservasServices: ReservasService){}
 
+  //Objeto de tipo empresa donde guardaremos los datos de la empresa con el que estamos operando
   empresa: Empresa = {
     nombre_empresa: '',
     logo: '',
@@ -20,16 +21,21 @@ export class SolicitudComponent {
     id_empresa: 0
   }
 
+  //Función que crea la solicitud para que el administrador general pueda verla
   crearSolicitud(nombre: any, datos_de_contacto: any, descripcion: any, logo: any){
+    //Si falta algún dato de la empresa, mostramos un mensaje de error
     if(nombre=='' || logo==''|| datos_de_contacto==''|| descripcion==''){
       confirm("Faltan parámetros");
     }else{
+      //Si no, guardamos los datos en el objeto empresa
       this.empresa.nombre_empresa = nombre;
       this.empresa.logo = logo;
       this.empresa.datos_de_contacto = datos_de_contacto;
       this.empresa.descripcion = descripcion;
+      //Llamamos a la función "crearSolicitud" que creará una solicitud y lo añadirá en la base de datos
       this.reservasServices.crearSolicitud("usuario", this.empresa).subscribe(
         res => {
+          //Por último nos movemos a la pantalla de login
           this.router.navigate(['/reservas/login']);
         },
         err => console.error(err)

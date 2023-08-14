23a4484fd6dde7a07e313a9307e02a156f84cbb3
teresacheca <teresa.checamarabotto@gmail.com>
2023-08-14 11:37:48 +0200

    Comentarios admi empresa datos recurso

--- frontend/src/app/ae-datos-recurso/ae-datos-recurso.component.ts
+++ frontend/src/app/ae-datos-recurso/ae-datos-recurso.component.ts
@@ -12,6 +12,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class AeDatosRecursoComponent {
   constructor(private reservasServices: ReservasService, private router: Router, private activeRoute: ActivatedRoute){}
 
+  //Objeto de tipo usuario donde guardaremos los datos del recurso con el que estamos operando
   recurso: Recurso = {
     nombre_rs: '',
     descripcion: '',
@@ -23,18 +24,24 @@ export class AeDatosRecursoComponent {
     id_empresa: 0
   }
 
+  //Inicialización de las variables que usaremos para el resto del código
   empresa: number = 0
   admi: string = ''
   id: number = 0
   aux: any
 
   ngOnInit(){
+    //Cogemos los parámetros que se leen en la url
     const params = this.activeRoute.snapshot.params;
     this.empresa = params["id_empresa"]
     this.admi = params["nombre_usuario"]
     this.id = params["id_recursoservicio"]
+
+    //Obtenemos todos los datos del recurso a partir de su id que obtenemos en la url
+    //Necesitamos saber también la empresa y el administrador
     this.reservasServices.getDatosRecursoAe(this.admi, this.empresa, this.id).subscribe(
       res =>{
+        //Copiamos todos los datos que nos devuelve la función en un objeto de tipo recurso que hemos llamado recurso
        this.aux = res
        this.recurso.nombre_rs = this.aux[0].nombre_rs
        this.recurso.descripcion = this.aux[0].descripcion
@@ -49,6 +56,7 @@ export class AeDatosRecursoComponent {
     );
   }
 
+  //Función que nos permite volver a la página anterior, es decir, a la página que muestra la lista de recursos de la empresa
   volver(){
     let ruta = '/reservas/admi_empresa/' + this.admi + '/' + this.empresa + '/lista_recursos'
     this.router.navigate([ruta])

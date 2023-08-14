e3b7b105993fe83b8dc228667aed630ad2061b22
teresacheca <teresa.checamarabotto@gmail.com>
2023-08-14 17:51:10 +0200

    Comentarios admi empresa lista usuarios

--- frontend/src/app/ae-lista-usuarios/ae-lista-usuarios.component.ts
+++ frontend/src/app/ae-lista-usuarios/ae-lista-usuarios.component.ts
@@ -15,18 +15,22 @@ export class AeListaUsuariosComponent {
   empresa: number = 0
   nombre_admi: string = ""
 
-  constructor(private reservasServices: ReservasService, private router: Router, private activeRoute: ActivatedRoute){
-
-  }
+  constructor(private reservasServices: ReservasService, private router: Router, private activeRoute: ActivatedRoute){}
 
 
   ngOnInit(){
+    //Cogemos los parámetros que se leen en la url
     const params = this.activeRoute.snapshot.params;
     this.empresa = params["id_empresa"]
     this.nombre_admi = params["nombre_usuario"]
+
+    //Obtenemos todos los usuarios de la empresa a partir de su identificador con la función getUsuariosEmpresaAe 
+    //necesitaremos también el nombre del administrador para poder acceder a la url correspondiente
     this.reservasServices.getUsuariosEmpresaAe(params["nombre_usuario"], params["id_empresa"]).subscribe(
       res =>{
         this.usuarios = res;
+        //Si no devuelve ningún usuario es porque esa empresa no tiene usuario 
+        //de forma que indicaremos que la lista de usuarios está vacía
         if(this.usuarios.length == 0){
           this.vacio = true
         }
@@ -35,16 +39,19 @@ export class AeListaUsuariosComponent {
     );
   }
 
+  //Función que nos permite ir a la página donde podremos editar los datos del usuario
   AeEditaUsuario(id: number){
     let ruta = this.router.url + '/' + id
     this.router.navigate([ruta]);
   }
 
+  //Función que nos permite ir a la página donde podremos añadir un nuevo usuario a la empresa
   AeAniadeUsuario(){
     let ruta = this.router.url + '/aniade'
     this.router.navigate([ruta]);
   }
 
+  //Función que nos permite volver a la página anterior, es decir, a la página que nos muestra la pantalla inicial del perfil del administrador de la empresa
   volver(){
     let ruta = '/reservas/admi_empresa/' + this.nombre_admi
     this.router.navigate([ruta])

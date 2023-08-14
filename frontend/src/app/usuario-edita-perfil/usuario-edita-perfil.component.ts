c00d9acf71e6aa51bd916d301ae1d6e3cc695257
teresacheca <teresa.checamarabotto@gmail.com>
2023-08-14 20:26:51 +0200

    Comentarios usuario edita perfil

--- frontend/src/app/usuario-edita-perfil/usuario-edita-perfil.component.ts
+++ frontend/src/app/usuario-edita-perfil/usuario-edita-perfil.component.ts
@@ -13,6 +13,7 @@ export class UsuarioEditaPerfilComponent {
 
   constructor(private reservaServices: ReservasService, private router: Router, private activeRoute: ActivatedRoute ){}
 
+  //Objeto de tipo usuario donde guardaremos los datos del usuario con el que estamos operando
   usuario: Usuario = {
     nombre_usuario: '',
     contrasena: '',
@@ -24,18 +25,23 @@ export class UsuarioEditaPerfilComponent {
     foto: "",
     id_empresa: 0
   }
-  fecha_nacimiento: string = ""
 
+  //Resto de variables que usaremos en el código
+  fecha_nacimiento: string = ""
   aux: any = []
   nombre: string = ""
   id_empresa: number = 0
+  mostrarContrasena = false;
 
   ngOnInit(){
+    //Cogemos los parámetros que se leen en la url
     const params = this.activeRoute.snapshot.params;
-    
     this.usuario.nombre_usuario = params["nombre_usuario"]
+
+    //Obtenemos los datos del usuario de la empresa que queremos mostrar a partir de su nombre
     this.reservaServices.getUsuario(this.usuario.nombre_usuario).subscribe(
       res => {
+        //Guardamos todos los datos del usuario
         this.aux = res
         this.usuario.nombre_usuario = this.aux[0].nombre_usuario
         this.nombre = this.usuario.nombre_usuario
@@ -55,28 +61,52 @@ export class UsuarioEditaPerfilComponent {
     
   }
 
+  //Función que guarda los datos modificados del usuario
   guardarCambiosUsuario(nombre_usuario: string, nuevoUsuario: Usuario, fecha: string){
+    //Copiamos en el nuevo usuario la nueva fecha de nacimiento para que no falle el formato
     nuevoUsuario.fecha_nacimiento = new Date(fecha)
-
+    
+    //Comprobamos que el nuevo nombre introducido no coincide con el de otro usuario
+    //Tambiñen comprueba su antiguo nombre para que no haya error de que coincida si el nombre no se modifica
+    //Llamamos a la función que nos devuelve todos los usuarios que haya con el nombre que le introducimos
     this.reservaServices.getUsuarioNombre(this.usuario.nombre_usuario).subscribe(
       res => {
         this.aux = res
+        //Comprueba que no haya otro usuario con ese nombre execepto el antiguo nombre de usuario
         if(this.aux.length > 0 && this.nombre != nuevoUsuario.nombre_usuario){
+          //Si ya hay otro usuario con ese nombre, mostramos un mensaje de error
           confirm("Ese nombre ya está en uso");
         }else{
+          //Si no, comprobamos el resto de datos
+
+          //Comprobamos si se ha modificado la empresa, si esta nueva empresa existe
+          //Llamamos a la función que nos devuelve los datos de la empresa en función del nombre que le pasemos
           this.reservaServices.getEmpresa(nombre_usuario, nuevoUsuario.empresa).subscribe(
             res => {
              this.aux = res
+             //Si la función no nos devuelve ninguna empresa, se entiende que el nombre introducido no corresponde con ninguna empresa
+             //y por tanto, mostramos un mensaje de error
              if(this.aux.length == 0){
               confirm("La empresa seleccionada no existe")
              }else{
+              //En caso contrario, comprobamos si la empresa nueva es distinta a la anterior
+
+              //Si es distinta, entendemos que el usuario se ha cambiado de empresa y que por tanto tenemos que borrar 
+              //toda la información que tenga en la empresa antigua
               if(this.id_empresa != this.aux[0].id_empresa){
+
+                //Llamamos a la función que nos elimina todas las reservas del usuario en función de su nombre y el identificador de la empresa
                 this.reservaServices.eliminarReservasUsuario(nuevoUsuario.nombre_usuario, this.id_empresa).subscribe(
                   res => {
+                    //Actualizamos el identificador de la empresa
                     nuevoUsuario.id_empresa = this.aux[0].id_empresa
-                    console.log(nuevoUsuario)
+                   
+                    //Una vez comprobado todo, ya podemos cambiar los datos del usuario
+                    //Llamamos a la función que actualiza los datos de una usuario a partir de su nombre y los nuevo datos
                     this.reservaServices.guardarCambiosUsuario(nombre_usuario, nuevoUsuario).subscribe(
                       res => {
+                        //Por último, nos movemos a la página que nos muestra la página principal de usuario
+                        //Comprobamos el tipo de usuario por si ha cambiado, ya que deependidendo del tipo nos moveremos a una dirección o a otra
                         if(nuevoUsuario.tipo == 0){
                           let ruta = '/reservas/admi_general/' + nombre_usuario
                           this.router.navigate([ruta]);
@@ -94,8 +124,12 @@ export class UsuarioEditaPerfilComponent {
                   err=> console.error(err)
                 )
               }else{
+                //Si no cambia la empresa, no tenemos que borrar el resto de datos y por tanto podemos actualizar directamente los 
+                //datos del usuario llamando a la misma función 
                 this.reservaServices.guardarCambiosUsuario(nombre_usuario, nuevoUsuario).subscribe(
                   res => {
+                    //Por último, nos movemos a la página que nos muestra la página principal de usuario
+                    //Comprobamos el tipo de usuario por si ha cambiado, ya que deependidendo del tipo nos moveremos a una dirección o a otra
                     if(nuevoUsuario.tipo == 0){
                       let ruta = '/reservas/admi_general/' + nombre_usuario
                       this.router.navigate([ruta]);
@@ -124,9 +158,12 @@ export class UsuarioEditaPerfilComponent {
     
   }
 
+  //Función que llama a la función para borrar la cuenta del usuario
   eliminarCuentaUsuarioUsu(nombre_usuario: string){
+    //Borramos la cuenta del usuario a partir de si nombre de usuario
     this.reservaServices.eliminarCuentaUsuarioUsu(nombre_usuario).subscribe(
       res => {
+        //Por último nos movemos a la página inicial de login
         let ruta = '/reservas/login'
         this.router.navigate([ruta]);
       },
@@ -135,13 +172,14 @@ export class UsuarioEditaPerfilComponent {
 
   }
 
+  //Función que nos permite volver a la página anterior, es decir, a la página principal del usuario
   volver(){
     let ruta = '/reservas/usuario/' + this.nombre 
     this.router.navigate([ruta])
   }
 
-  mostrarContrasena = false;
   
+  //Función que cambia el valor de mostrarContrasena (cada vez que pulsamos el botón de ver) que nos permite ver la contraseña o verla cifrada
   verContrasena() {
     this.mostrarContrasena = !this.mostrarContrasena;
   }

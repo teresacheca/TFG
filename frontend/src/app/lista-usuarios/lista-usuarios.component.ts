fc7b56db400766bc75a7ff130290000e5dee04b7
teresacheca <teresa.checamarabotto@gmail.com>
2023-08-14 19:50:30 +0200

    Comentarios admi general lista usuarios

--- frontend/src/app/lista-usuarios/lista-usuarios.component.ts
+++ frontend/src/app/lista-usuarios/lista-usuarios.component.ts
@@ -10,23 +10,28 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./lista-usuarios.component.css']
 })
 export class ListaUsuariosComponent {
+
+  constructor(private reservasServices: ReservasService, private router: Router, private activeRoute: ActivatedRoute){}
+
+  //Variables que usaremos en el resto del código
+  empresa: number = 0
+  nombre_admi: string = ""
   usuarios: any = []
   vacio = false
 
-  constructor(private reservasServices: ReservasService, private router: Router, private activeRoute: ActivatedRoute){
-
-  }
-
-  empresa: number = 0
-  nombre_admi: string = ""
-
   ngOnInit(){
+    //Cogemos los parámetros que se leen en la url
     const params = this.activeRoute.snapshot.params;
     this.empresa = params["id_empresa"]
     this.nombre_admi = params["nombre_usuario"]
+
+    //Obtenemos todos los usuarios de la empresa a partir de su identificador con la función getUsuariosEmpresa 
+    //necesitaremos también el nombre del administrador para poder acceder a la url correspondiente
     this.reservasServices.getUsuariosEmpresa(this.nombre_admi, this.empresa).subscribe(
       res =>{
         this.usuarios = res;
+        //Si no devuelve ningún usuario es porque esa empresa no tiene usuarios 
+        //de forma que indicaremos que la lista de usuarios está vacía
         if(this.usuarios.length == 0){
           this.vacio = true
         }
@@ -35,11 +40,13 @@ export class ListaUsuariosComponent {
     );
   }
 
+  //Función que nos permite ir a la página donde podremos ver los datos del usuario
   verUsuario(id: number){
     let ruta = '/reservas/' + this.nombre_admi + '/empresas/' + this.empresa + '/lista_usuarios/' + id
     this.router.navigate([ruta]);
   }
 
+  //Función que nos permite volver a la página anterior, es decir, a la página que nos muestra la pantalla inicial del perfil del administrador general
   volver(){
     let ruta = '/reservas/' + this.nombre_admi + '/empresas/' + this.empresa 
     this.router.navigate([ruta])
